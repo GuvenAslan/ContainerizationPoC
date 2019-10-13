5e8167310238763d04e34a97dbccab04a7c28a7c
Guven Aslan <guvennaslan@gmail.com>
2019-10-13 16:02:46 +0300

    ContainersPoC Init

---
+++ ContainersPoC/ContainersPoC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ContainersPoC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
